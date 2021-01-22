cafe34b17e88a399f3d50378d15a200155b16625
Bronley <bronley@gmail.com>
2021-01-22 17:07:26 -0500

    Add local storage saving and loading

--- public/index.js
+++ public/index.js
@@ -1,3 +1,34 @@
+$(function () {
+    var id = getParameterByName('id');
+    if (id) {
+        //load suite from local storage
+        var suite = JSON.parse(localStorage.getItem(id));
+        //populate the form with the suite data
+        $("#id").val(suite.id);
+        $("#author").val(suite.author);
+        $('#author').val(suite.author);
+        $('#authorEmail').val(suite.email);
+        $('#authorURL').val(suite.url);
+        $('#title').val(suite.title);
+        $('#info').val(suite.info);
+        $('#initHTML').val(suite.initHTML);
+        $('#setup').val(suite.setup);
+        $('#teardown').val(suite.teardown);
+        for (var i = 1; i <= suite.tests.length; i++) {
+            var prefix = 'test[' + i + ']';
+            var test = suite.tests[i - 1];
+            //create a new snippet
+            $("#add-test").click();
+            document.getElementById(prefix + '[title]').value = test.title;
+            document.getElementById(prefix + '[defer]').checked = test.defer;
+            document.getElementById(prefix + '[code]').value = test.code;
+        }
+    } else {
+        //populate two test snippets
+        $("#add-test").click().click();
+    }
+});
+
 
 function getFormData() {
     var form = $('form').first();
@@ -17,15 +48,25 @@ function getFormData() {
         suite.id = nanoid();
     }
 
-    var testElements = $('.test');
-    for (var i = 0; i < testElements.length; i++) {
-        var prefix = 'test[' + i + ']';
-        suite.tests.push({
-            title: document.getElementById(prefix + '[title]').value,
-            defer: document.getElementById(prefix + '[defer]').value,
-            code: document.getElementById(prefix + '[code]').value
-        });
+    var i = 1;
+    var snippetData;
+    while (snippetData = getSnippetData(i++)) {
+        suite.tests.push(snippetData);
     }
 
-    console.log(suite);
+    //save the suite to local storage
+    localStorage.setItem(suite.id, JSON.stringify(suite));
+    console.log(suite.id, suite);
+}
+
+function getSnippetData(index) {
+    var prefix = 'test[' + index + ']';
+    var titleElement = document.getElementById(prefix + '[title]');
+    if (titleElement) {
+        return {
+            title: titleElement.value,
+            defer: document.getElementById(prefix + '[defer]').checked,
+            code: document.getElementById(prefix + '[code]').value
+        };
+    }
 }
